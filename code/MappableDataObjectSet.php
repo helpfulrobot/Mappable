a2602542380327108bc29dbe53fc9622da059521
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-01 21:57:16 +0700

    ENHANCEMENT: Allow rendering of markers and for it to be optional

--- code/MappableDataObjectSet.php
+++ code/MappableDataObjectSet.php
@@ -8,13 +8,30 @@
  */
 class MappableDataObjectSet extends Extension {
 
+	private $RenderMarkers = true;
+
+	public function setRenderMarkers($newRenderMarkers) {
+		$this->RenderMarkers = $newRenderMarkers;
+	}
+
 	public function getRenderableMap($width = null, $height = null) {
 		$gmap = MapUtil::get_map($this->owner);
 		$w = $width ? $width : MapUtil::$map_width;
 		$h = $height ? $height : MapUtil::$map_height;
 		$gmap->setSize($w,$h);
+
+		if ($this->RenderMarkers == true) {
+			foreach ($this->owner->getIterator() as $marker) {
+				$gmap->addMarkerAsObject($marker);
+			}
+		}
+
 		return $gmap;
 	}
 
 
+	public function getMappableCacheKey() {
+		$key = 'mappabledataobjectset_'.$this->ID.'_'.$this->LastEdited;
+		$key .= $this->owner->max('LastEdited');
+	}
 }
